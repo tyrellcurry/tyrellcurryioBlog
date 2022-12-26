95d56543d482eea942de6d9991ec1f2f85303aad
Tyrell C <tyrellrcurry@gmail.com>
2022-12-25 20:48:04 -0800

    First commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Netlify Widget */}
+          <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+          `}}/>
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
