786b7bd2eddbd185714fe76f8d355474aa139354
Tyrell C <tyrellrcurry@gmail.com>
2023-02-01 18:55:29 -0800

    REBUILDING FROM SCRATCH - Moved from NextJS to AstroJS. - Built a darkmode toggle that saves preference to LocalStorage - Tailwind CSS installed - Housekeeping

---
+++ src/scripts/dm.js
@@ -0,0 +1,5 @@
+function dm() {
+    const htmlElement = document.querySelector('html');
+    localStorage.theme == "dark" ? (htmlElement.classList.remove('dark'), console.log('Welcome to the light ✨'), localStorage.theme = "light") : (htmlElement.classList.add('dark'), console.log('Welcome to the dark 🌘'), localStorage.theme = "dark");
+}
+window.dm = dm;
